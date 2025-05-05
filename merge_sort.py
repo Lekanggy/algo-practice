d7af22b049dcb6a9f672253d29f8021e348c4e1b
Lekanggy <lekanggy@gmail.com>
2025-05-05 15:55:46 +0100

    python implememtation of mergesort

--- merge_sort.py
+++ merge_sort.py
@@ -8,24 +8,48 @@ def merge(array, startLeft, endLeft, end):
     startRight = endLeft + 1
 
     # create a new temporary storage
-    arrLeft = []
-    arrRight = []
+    arrLeft = [None] * (sizeLeft + 1)
+    arrRight = [None] * (sizeRight + 1)
 
     #copy the array into the temporary storage
     for i in range(sizeLeft):
-        print(i)
-        arrLeft[i] = array[startLeft]
-    #arrLeft[sizeLeft] = sys.maxsize
+        arrLeft[i] = array[startLeft + i]
+    arrLeft[sizeLeft] = sys.maxsize
 
     for i in range(sizeRight):
-        print("aa", i)
-        #arrRight[i] = array[startRight + i]
+        arrRight[i] = array[startRight + i]
+    arrRight[sizeRight] = sys.maxsize
 
-    #arrRight[sizeRight] = sys.maxsize
+    indexLeft = 0
+    indexRight = 0
+    for i in range(startLeft, end + 1):
+        if arrLeft[indexLeft] <= arrRight[indexRight]:
+            array[i] = arrLeft[indexLeft]
+            indexLeft = indexLeft + 1
+        else:
+            array[i] = arrRight[indexRight]
+            indexRight = indexRight + 1
+
+    print("r", arrRight)
+    print("L", arrLeft)
+    print("arr", array)
     pass
+
+def mergeSort(array, start, end):
+    if(start < end):
+        endLeft = (start + end) // 2
+        mergeSort(array, start, endLeft)
+        mergeSort(array, endLeft + 1, end)
+        merge(array, start, endLeft, end)
+
+def mergeSortWrapper(array):
+    size = len(array)
+    mergeSort(array, 0, size - 1)
    
 
 arr = [43, 24, 45, 27, 35, 47, 22, 48]
 
-merge(arr, 0, 3, 7)
+#merge(arr, 0, 3, 7)
+mergeSortWrapper(arr)
+print(arr)
   
